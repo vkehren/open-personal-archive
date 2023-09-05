8cc6621e87b465bcf67d5a497b29157e0c3998cb
rehrenreich <ryanse@gmail.com>
2023-09-05 04:14:29 -0700

    * Renamed Authorization file to AccessRequests. * Deleted "helloWorld" Firebase function.

--- code/functions/src/index.ts
+++ code/functions/src/index.ts
@@ -1,4 +1,3 @@
-import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as OPA from "../../base/src";
 import * as adminCredentialFile from "../open-personal-archive-firebase-adminsdk-credential.json";
@@ -16,15 +15,4 @@ if (useDefaultAdminCredential) {
 
 // NOTE: Export API functions
 export * from "./Application";
-
-export const helloWorld = functions.https.onCall(async (data, context) => { // eslint-disable-line @typescript-eslint/no-unused-vars
-  try {
-    const localizableHelloValues = {"en": "Hello!", "es": "¡Hola!"};
-    const locale = (Math.random() >= .5) ? "en" : "es";
-    const helloText = OPA.getLocalizedText(localizableHelloValues, locale);
-
-    return OPA.getSuccessResult(helloText, data);
-  } catch (error) {
-    return OPA.getFailureResult(error as Error);
-  }
-});
+export * from "./AccessRequests";
