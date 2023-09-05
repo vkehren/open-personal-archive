1881e356897d11299a5be4da54e8192fb91af2e5
rehrenreich <ryanse@gmail.com>
2023-09-05 03:04:15 -0700

    * Renamed Authorization file to AccessRequests.

--- code/domainlogic/src/index.ts
+++ code/domainlogic/src/index.ts
@@ -1,4 +1,4 @@
 export const name = "open-personal-archive-domain-logic";
 
 export * as Application from "./system/Application";
-export * as Authorization from "./Authorization";
+export * as AccessRequests from "./AccessRequests";
