d35ad6b0fdcf766cbf509e475ae6b146d01cc08d
rehrenreich <ryanse@gmail.com>
2023-09-05 13:04:27 -0700

    * Improved code to configure admin Firebase app. * Added code to configure Express app, including Cross-Origin Resource Sharing (CORS).

--- code/functions/src/index.ts
+++ code/functions/src/index.ts
@@ -1,16 +1,41 @@
 import * as admin from "firebase-admin";
 import * as OPA from "../../base/src";
 import * as adminCredentialFile from "../open-personal-archive-firebase-adminsdk-credential.json";
+import express = require("express");
+import cors = require("cors");
 
-// NOTE: Initialize Admin App
-export const useDefaultAdminCredential = false;
-if (useDefaultAdminCredential) {
-  admin.initializeApp();
-} else {
+const useAdminCredentialFile = true;
+const useJsonParser = true;
+const useUrlEncodingParser = true;
+const useCorsHandler = true;
+const useCorsProductionSettings = false;
+
+// CONFIGURE ADMIN APP
+let adminAppOptions = (undefined as admin.AppOptions | undefined);
+if (useAdminCredentialFile) {
   const adminCredential = OPA.convertFirebaseKeyToCredential(adminCredentialFile);
-  admin.initializeApp({
-    credential: admin.credential.cert(adminCredential),
-  });
+  adminAppOptions = {credential: admin.credential.cert(adminCredential)};
+}
+admin.initializeApp(adminAppOptions);
+
+// CONFIGURE EXPRESS APP
+const expressApp = express();
+if (useJsonParser) {
+  const jsonParser = express.json();
+  expressApp.use(jsonParser);
+}
+if (useUrlEncodingParser) {
+  const urlEncodingOptions = {extended: true}; // NOTE: Allows for encoding arrays and objects
+  const urlEncodingParser = express.urlencoded(urlEncodingOptions);
+  expressApp.use(urlEncodingParser);
+}
+if (useCorsHandler) {
+  // NOTE: "origin" value of "true" means reflect origin of sender, so even calls from localhost or local file loaded in browser will work
+  const corsOptionsForDevelopment = {origin: true};
+  const corsOptionsForProduction = {origin: ["https://YOUR_DOMAIN_NAME_1", "https://YOUR_DOMAIN_NAME_2", "https://YOUR_DOMAIN_NAME_3"]};
+  const corsOptions = (useCorsProductionSettings) ? corsOptionsForProduction : corsOptionsForDevelopment;
+  const corsHandler = cors(corsOptions);
+  expressApp.use(corsHandler);
 }
 
 // NOTE: Export API functions
