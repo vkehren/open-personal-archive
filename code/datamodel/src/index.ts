fe2156714d60cb2347a3678bba484ba418399573
rehrenreich <ryanse@gmail.com>
2023-09-05 13:21:12 -0700

    * Re-organized lines of code to group them by functional type.

--- code/datamodel/src/index.ts
+++ code/datamodel/src/index.ts
@@ -1,13 +1,16 @@
 import * as OPA from "../../base/src";
-import * as AccessRequest from "./doctypes/AccessRequest";
-import * as Archive from "./doctypes/Archive";
+// System Required
 import * as AuthProvider from "./doctypes/AuthenticationProvider";
 import * as Locale from "./doctypes/Locale";
-import * as Application from "./doctypes/Application";
 import * as Role from "./doctypes/Role";
 import * as TimeZone from "./doctypes/TimeZone";
 import * as TimeZoneGroup from "./doctypes/TimeZoneGroup";
+// System Created
+import * as Application from "./doctypes/Application";
+import * as Archive from "./doctypes/Archive";
 import * as User from "./doctypes/User";
+// User Created
+import * as AccessRequest from "./doctypes/AccessRequest";
 
 export const name = "open-personal-archive-data-model";
 export const schemaVersion = "0.0.0.1";
@@ -17,41 +20,41 @@ export const schemaVersion = "0.0.0.1";
 // NOTE: If the CollectionDescriptor represents data that will dynamically be created during usage of system, expose factory function via ITypedFactoryCollectionDescriptor<T, F> interface
 //         -> And for special case instances (e.g. User who is Archive Owner), export the special case factory function directly later below
 export interface IOpaDbDescriptor extends OPA.ICollection {
-  readonly AccessRequests: OPA.ITypedQueryableFactoryCollectionDescriptor<AccessRequest.IAccessRequest, AccessRequest.AccessRequestQuerySet, AccessRequest.FactoryFunc>;
-  readonly Archive: OPA.ITypedQueryableCollectionDescriptor<Archive.IArchive, Archive.QuerySet>;
   readonly AuthProviders: OPA.ITypedQueryableCollectionDescriptor<AuthProvider.IAuthenticationProvider, AuthProvider.AuthenticationProviderQuerySet>;
   readonly Locales: OPA.ITypedQueryableCollectionDescriptor<Locale.ILocale, Locale.QuerySet>;
-  readonly Application: OPA.ITypedQueryableCollectionDescriptor<Application.IApplication, Application.QuerySet>;
   readonly Roles: OPA.ITypedQueryableCollectionDescriptor<Role.IRole, Role.QuerySet>;
   readonly TimeZones: OPA.ITypedQueryableCollectionDescriptor<TimeZone.ITimeZone, TimeZone.QuerySet>;
   readonly TimeZoneGroups: OPA.ITypedQueryableCollectionDescriptor<TimeZoneGroup.ITimeZoneGroup, TimeZoneGroup.QuerySet>;
+  readonly Application: OPA.ITypedQueryableCollectionDescriptor<Application.IApplication, Application.QuerySet>;
+  readonly Archive: OPA.ITypedQueryableCollectionDescriptor<Archive.IArchive, Archive.QuerySet>;
   readonly Users: OPA.ITypedQueryableFactoryCollectionDescriptor<User.IUser, User.UserQuerySet, User.FactoryFunc>;
+  readonly AccessRequests: OPA.ITypedQueryableFactoryCollectionDescriptor<AccessRequest.IAccessRequest, AccessRequest.AccessRequestQuerySet, AccessRequest.FactoryFunc>;
   readonly RootCollections: Array<OPA.ICollectionDescriptor>;
   readonly NestedCollections: Array<OPA.ICollectionDescriptor>;
 }
 
 const DbDescriptor = (OPA.CollectionDescriptors as OPA.ICollection);
-DbDescriptor.AccessRequests = AccessRequest.CollectionDescriptor;
-DbDescriptor.Archive = Archive.CollectionDescriptor;
 DbDescriptor.AuthProviders = AuthProvider.CollectionDescriptor;
 DbDescriptor.Locales = Locale.CollectionDescriptor;
-DbDescriptor.Application = Application.CollectionDescriptor;
 DbDescriptor.Roles = Role.CollectionDescriptor;
 DbDescriptor.TimeZones = TimeZone.CollectionDescriptor;
 DbDescriptor.TimeZoneGroups = TimeZoneGroup.CollectionDescriptor;
+DbDescriptor.Application = Application.CollectionDescriptor;
+DbDescriptor.Archive = Archive.CollectionDescriptor;
 DbDescriptor.Users = User.CollectionDescriptor;
+DbDescriptor.AccessRequests = AccessRequest.CollectionDescriptor;
 DbDescriptor.RootCollections = OPA.getCollectionFromObject<OPA.ICollectionDescriptor>(DbDescriptor, (colDesc) => (!OPA.isNullish(colDesc.collectionName)), (colDesc) => (!colDesc.isNestedCollection));
 DbDescriptor.NestedCollections = OPA.getCollectionFromObject<OPA.ICollectionDescriptor>(DbDescriptor, (colDesc) => (!OPA.isNullish(colDesc.collectionName)), (colDesc) => (colDesc.isNestedCollection));
 export const OpaDbDescriptor = OPA.convertTo<IOpaDbDescriptor>(DbDescriptor);
 
-export {IAccessRequest, IAccessRequestPartial} from "./doctypes/AccessRequest";
-export {IArchive, IArchivePartial, createSingleton as createArchive, SingletonId as ArchiveId} from "./doctypes/Archive";
-export {IAuthenticationProvider, AuthenticationProvider_GoogleId, AuthenticationProvider_RequiredIds} from "./doctypes/AuthenticationProvider"; // eslint-disable-line camelcase
+export {DefaultIndexCollection, localizableStringConstructor, ApprovalState, ApprovalStates, RoleType, RoleTypes} from "./BaseTypes";
 export {AuthorizationState, ISystemState, IAuthenticationState, IAuthorizationState, ICallState, IDataStorageState} from "./CallStateTypes";
+export {IAuthenticationProvider, AuthenticationProvider_GoogleId, AuthenticationProvider_RequiredIds} from "./doctypes/AuthenticationProvider"; // eslint-disable-line camelcase
 export {ILocale, DefaultLocaleId, DefaultLocale} from "./doctypes/Locale";
-export {IApplication, createSingleton as createApplication, SingletonId as ApplicationId} from "./doctypes/Application";
 export {IRole, Role_OwnerId, Role_AdministratorId, Role_EditorId, Role_ViewerId, Role_GuestId, Role_RequiredIds} from "./doctypes/Role"; // eslint-disable-line camelcase
 export {ITimeZone} from "./doctypes/TimeZone";
 export {ITimeZoneGroup} from "./doctypes/TimeZoneGroup";
+export {IApplication, createSingleton as createApplication, SingletonId as ApplicationId} from "./doctypes/Application";
+export {IArchive, IArchivePartial, createSingleton as createArchive, SingletonId as ArchiveId} from "./doctypes/Archive";
 export {IUser, IUserPartial, createArchiveOwner, User_OwnerId} from "./doctypes/User"; // eslint-disable-line camelcase
-export {DefaultIndexCollection, localizableStringConstructor, ApprovalState, ApprovalStates, RoleType, RoleTypes} from "./BaseTypes";
+export {IAccessRequest, IAccessRequestPartial} from "./doctypes/AccessRequest";
