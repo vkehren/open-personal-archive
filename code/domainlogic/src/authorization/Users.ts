b3022a69089c3ea3a5d7acddef0b2950e5b0fd55
rehrenreich <ryanse@gmail.com>
2023-09-06 15:56:48 -0700

    * Implemented initializeUserAccount(...) function in Users file to specifically create the corresponding User account data for the current authenticated Firebase user.

--- code/domainlogic/src/authorization/Users.ts
+++ code/domainlogic/src/authorization/Users.ts
@@ -1,5 +1,6 @@
 import * as OPA from "../../../base/src";
 import * as OpaDm from "../../../datamodel/src";
+import {OpaDbDescriptor as OpaDb} from "../../../datamodel/src";
 import * as Application from "../system/Application";
 
 export interface IAnonymousDisplayModel {
@@ -58,13 +59,77 @@ export async function getUserAccountDisplayModel(callState: OpaDm.ICallState | n
   }
 
   OPA.assertNonNullish(callStateNonNull.authorizationState, "The Authorization State must not be null.");
-  const authorizationStateNonNull = OPA.convertNonNullish(callStateNonNull.authorizationState);
+  const authorizationState = OPA.convertNonNullish(callStateNonNull.authorizationState);
 
   const userAccountDisplayModel: IUserAccountDisplayModel = {
     isAnonymous: false,
     firebaseAuthState: callStateNonNull.authenticationState,
     isUserAccountInitialized: true,
-    userAccount: authorizationStateNonNull.user,
+    userAccount: authorizationState.user,
   };
   return userAccountDisplayModel;
+}
+
+/**
+ * Initializes the account data for the current User using the Open Personal Archive™ (OPA) system.
+ * @param {OpaDm.ICallState} callState The Call State for the current User.
+ * @param {string} authProviderId The Firebase Auth id of the authentication provider used to authenticate the current Firebase Auth user account.
+ * @param {string} authAccountName The Firebase Auth name of the authentication account used to authenticate the current Firebase Auth user account.
+ * @return {Promise<OpaDm.IUser>}
+ */
+export async function initializeUserAccount(callState: OpaDm.ICallState, authProviderId: string, authAccountName: string): Promise<OpaDm.IUser> {
+  OPA.assertNonNullish(callState, "The Call State must not be null.");
+  OPA.assertNonNullish(callState.dataStorageState, "The Data Storage State must not be null.");
+  OPA.assertFirestoreIsNotNullish(callState.dataStorageState.db);
+
+  const isSystemInstalled = await Application.isSystemInstalled(callState.dataStorageState);
+  OPA.assertSystemIsInstalled(isSystemInstalled);
+  OPA.assertNonNullish(callState.authenticationState, "The Authentication State must not be null.");
+  OPA.assertNonNullish(callState.systemState, "The System State must not be null.");
+  OPA.assertIsFalse(callState.hasAuthorizationState, "The User account has already been initialized.");
+
+  const authenticationState = OPA.convertNonNullish(callState.authenticationState);
+  const systemState = OPA.convertNonNullish(callState.systemState);
+  const db = callState.dataStorageState.db;
+
+  const preExistingUser = OpaDb.Users.queries.getByFirebaseAuthUserId(db, authenticationState.firebaseAuthUserId);
+  const hasPreExistingUser = (!OPA.isNullish(preExistingUser));
+  OPA.assertIsFalse(hasPreExistingUser, "The User account has already been initialized, but possibly with errors.");
+
+  const firebaseAuthUserId = authenticationState.firebaseAuthUserId;
+  const firstName = (!OPA.isNullishOrWhitespace(authenticationState.firstName)) ? OPA.convertNonNullish(authenticationState.firstName) : "";
+  const lastName = (!OPA.isNullishOrWhitespace(authenticationState.lastName)) ? OPA.convertNonNullish(authenticationState.lastName) : "";
+
+  const authProvider = await OpaDb.AuthProviders.queries.getByExternalAuthProviderId(db, authProviderId);
+  OPA.assertDocumentIsValid(authProvider, "The required AuthProvider does not exist.");
+  const assignedRole = await OpaDb.Roles.queries.getById(db, OpaDm.DefaultRoleId);
+  OPA.assertDocumentIsValid(assignedRole, "The required Role does not exist.");
+  const locale = await OpaDb.Locales.queries.getById(db, systemState.archive.defaultLocaleId);
+  OPA.assertDocumentIsValid(locale, "The required Locale does not exist.");
+  const timeZoneGroup = await OpaDb.TimeZoneGroups.queries.getById(db, systemState.archive.defaultTimeZoneGroupId);
+  OPA.assertDocumentIsValid(timeZoneGroup, "The required TimeZoneGroup does not exist.");
+
+  const userCollectionRef = OpaDb.Users.getTypedCollection(db);
+  const userDocumentRef = userCollectionRef.doc();
+  const userId = userDocumentRef.id;
+  const user = OpaDb.Users.createInstance(userId, firebaseAuthUserId, OPA.convertNonNullish(authProvider), authAccountName, OPA.convertNonNullish(assignedRole), OPA.convertNonNullish(locale), OPA.convertNonNullish(timeZoneGroup), firstName, lastName);
+
+  const batchUpdate = db.batch(); // REPLACES: await userDocumentRef.set(user, {merge: true});
+  batchUpdate.set(userDocumentRef, user, {merge: true});
+  const firebaseAuthUserIdIndexCollectionRef = db.collection(OpaDm.Index_User_FirebaseAuthUserId.indexCollectionName);
+  const firebaseAuthUserIdIndexDocRef = firebaseAuthUserIdIndexCollectionRef.doc(OpaDm.Index_User_FirebaseAuthUserId.getDocumentId(user.firebaseAuthUserId));
+  batchUpdate.set(firebaseAuthUserIdIndexDocRef, {value: user.id});
+  const authAccountNameIndexCollectionRef = db.collection(OpaDm.Index_User_AuthAccountName.indexCollectionName);
+  const authAccountNameIndexDocRef = authAccountNameIndexCollectionRef.doc(OpaDm.Index_User_AuthAccountName.getDocumentId(user.authAccountName));
+  batchUpdate.set(authAccountNameIndexDocRef, {value: user.id});
+  const authAccountNameLoweredIndexCollectionRef = db.collection(OpaDm.Index_User_AuthAccountNameLowered.indexCollectionName);
+  const authAccountNameLoweredIndexDocRef = authAccountNameLoweredIndexCollectionRef.doc(OpaDm.Index_User_AuthAccountNameLowered.getDocumentId(user.authAccountNameLowered));
+  batchUpdate.set(authAccountNameLoweredIndexDocRef, {value: user.id});
+  await batchUpdate.commit();
+
+  const userReRead = await OpaDb.Users.queries.getById(db, userId);
+  OPA.assertDocumentIsValid(userReRead, "The requested User does not exist.");
+  const userReReadNonNull = OPA.convertNonNullish(userReRead);
+
+  return userReReadNonNull;
 }
