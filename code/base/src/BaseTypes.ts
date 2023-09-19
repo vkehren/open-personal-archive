acba5719f174beea8cd2e8ed6fb129f41bb3538b
rehrenreich <ryanse@gmail.com>
2023-09-19 14:21:53 -0700

    * Added "Id" type that aliases "string" in BaseTypes file. * Added "IdNullable" type that aliases "string | null | undefined" in BaseTypes file. * Moved "DateToUse" and "nowToUse" to top of BaseTypes file. * Used these types in interface and function definitions in BaseTypes file.

--- code/base/src/BaseTypes.ts
+++ code/base/src/BaseTypes.ts
@@ -2,6 +2,21 @@ import * as firestore from "@google-cloud/firestore";
 import * as TC from "./TypeChecking";
 import * as VC from "./ValueChecking";
 
+export type Id = string;
+export type IdNullable = Id | null | undefined;
+/**
+ * Use this Date type while issue with Firebase Firestore Timestamps is still unresolved (see https://github.com/jloosli/node-firestore-import-export/issues/46)
+ * @type
+ */
+export type DateToUse = Date;
+/**
+ * Use this Date now() function while issue with Firebase Firestore Timestamps is still unresolved (see https://github.com/jloosli/node-firestore-import-export/issues/46)
+ * @constant
+ * @type {function}
+ * @return {DateToUse} The current date and time.
+ */
+export const nowToUse = (): DateToUse => firestore.Timestamp.now().toDate();
+
 export type KeyText = string; // NOTE: " | symbol" causes Firebase Firestore query errors
 export type TypedKeyText<T> = (keyof T) & (KeyText);
 export type ContainerOfTypedKeyText<T> = {
@@ -13,7 +28,7 @@ export type ProxyFunc<T> = (obj: T) => T;
 export type GetterFunc<T, V> = (value: T, propName?: string | symbol) => V;
 export type GuardFunc<T> = (value: T) => boolean;
 export type FilterFunc<T> = (value: T) => boolean;
-export type IdFunc<T> = (value: T) => string | null | undefined;
+export type IdFunc<T> = (value: T) => IdNullable;
 export type ComparisonFunc<T> = (item1: T, item2: T) => number; // NOTE: (item1 < item2) => -1, (item1 == item2) => 0, else 1
 export type MappingFunc<T1, T2> = (item: T1) => T2;
 
@@ -87,23 +102,10 @@ export interface ITimeRangeCollection {
 /* eslint-disable camelcase */
 // NOTE: The types and interfaces below are useful for creating typed Firebase Firestore Documents
 
-/**
- * Use this Date type while issue with Firebase Firestore Timestamps is still unresolved (see https://github.com/jloosli/node-firestore-import-export/issues/46)
- * @type
- */
-export type DateToUse = Date;
-/**
- * Use this Date now() function while issue with Firebase Firestore Timestamps is still unresolved (see https://github.com/jloosli/node-firestore-import-export/issues/46)
- * @constant
- * @type {function}
- * @return {DateToUse} The current date and time.
- */
-export const nowToUse = (): DateToUse => firestore.Timestamp.now().toDate();
-
 // IDocument
 export const IDocument_DocumentId_PropertyName = VC.getTypedPropertyKeyAsText<IDocument>("id"); // eslint-disable-line camelcase
 export interface IDocument {
-  readonly id: string,
+  readonly id: Id,
 }
 
 // ICreatable
@@ -114,11 +116,11 @@ export interface ICreatable {
 }
 export const ICreatable_ByUser_UserIdOfCreator_PropertyName = VC.getTypedPropertyKeyAsText<ICreatable_ByUser>("userIdOfCreator"); // eslint-disable-line camelcase
 export interface ICreatable_ByUser extends ICreatable {
-  readonly userIdOfCreator: string;
+  readonly userIdOfCreator: Id;
 }
 export const ICreatable_ByNullableUser_UserIdOfCreator_PropertyName = VC.getTypedPropertyKeyAsText<ICreatable_ByNullableUser>("userIdOfCreator"); // eslint-disable-line camelcase
 export interface ICreatable_ByNullableUser extends ICreatable {
-  readonly userIdOfCreator: string | null;
+  readonly userIdOfCreator: Id | null;
 }
 export interface IDocument_Creatable extends IDocument, ICreatable { }
 export interface IDocument_Creatable_ByUser extends IDocument_Creatable, ICreatable_ByUser { }
@@ -133,7 +135,7 @@ export interface IUpgradeable {
 }
 export const IUpgradeable_ByUser_UserIdOfLatestUpgrader_PropertyName = VC.getTypedPropertyKeyAsText<IUpgradeable_ByUser>("userIdOfLatestUpgrader"); // eslint-disable-line camelcase
 export interface IUpgradeable_ByUser extends IUpgradeable {
-  readonly userIdOfLatestUpgrader: string | null;
+  readonly userIdOfLatestUpgrader: Id | null;
 }
 export interface IDocument_Upgradeable extends IDocument, IUpgradeable { }
 export interface IDocument_Upgradeable_ByUser extends IDocument_Upgradeable, IUpgradeable_ByUser { }
@@ -147,7 +149,7 @@ export interface IUpdateable {
 }
 export const IUpdateable_ByUser_UserIdOfLatestUpdater_PropertyName = VC.getTypedPropertyKeyAsText<IUpdateable_ByUser>("userIdOfLatestUpdater"); // eslint-disable-line camelcase
 export interface IUpdateable_ByUser extends IUpdateable {
-  readonly userIdOfLatestUpdater: string | null;
+  readonly userIdOfLatestUpdater: Id | null;
 }
 export interface IDocument_Updateable extends IDocument, IUpdateable { }
 export interface IDocument_Updateable_ByUser extends IDocument_Updateable, IUpdateable_ByUser { }
@@ -161,7 +163,7 @@ export interface IAssignableToRole {
 }
 export const IAssignableToRole_ByUser_UserIdOfLatestRoleAssigner_PropertyName = VC.getTypedPropertyKeyAsText<IAssignableToRole_ByUser>("userIdOfLatestRoleAssigner"); // eslint-disable-line camelcase
 export interface IAssignableToRole_ByUser extends IAssignableToRole {
-  readonly userIdOfLatestRoleAssigner: string | null;
+  readonly userIdOfLatestRoleAssigner: Id | null;
 }
 export interface IDocument_AssignableToRole extends IDocument, IAssignableToRole { }
 export interface IDocument_AssignableToRole_ByUser extends IDocument_AssignableToRole, IAssignableToRole_ByUser { }
@@ -175,7 +177,7 @@ export interface ITaggable {
 }
 export const ITaggable_ByUser_UserIdOfLatestTagger_PropertyName = VC.getTypedPropertyKeyAsText<ITaggable_ByUser>("userIdOfLatestTagger"); // eslint-disable-line camelcase
 export interface ITaggable_ByUser extends ITaggable {
-  readonly userIdOfLatestTagger: string | null;
+  readonly userIdOfLatestTagger: Id | null;
 }
 export interface IDocument_Taggable extends IDocument, ITaggable { }
 export interface IDocument_Taggable_ByUser extends IDocument_Taggable, ITaggable_ByUser { }
@@ -189,7 +191,7 @@ export interface IArchivable {
 }
 export const IArchivable_ByUser_UserIdOfArchivalChanger_PropertyName = VC.getTypedPropertyKeyAsText<IArchivable_ByUser>("userIdOfArchivalChanger"); // eslint-disable-line camelcase
 export interface IArchivable_ByUser extends IArchivable {
-  readonly userIdOfArchivalChanger: string | null;
+  readonly userIdOfArchivalChanger: Id | null;
 }
 export interface IDocument_Archivable extends IDocument, IArchivable { }
 export interface IDocument_Archivable_ByUser extends IDocument_Archivable, IArchivable_ByUser { }
@@ -203,7 +205,7 @@ export interface IViewable {
 }
 export const IViewable_ByUser_UserIdOfLatestViewer_PropertyName = VC.getTypedPropertyKeyAsText<IViewable_ByUser>("userIdOfLatestViewer"); // eslint-disable-line camelcase
 export interface IViewable_ByUser extends IViewable {
-  readonly userIdOfLatestViewer: string | null;
+  readonly userIdOfLatestViewer: Id | null;
 }
 export interface IDocument_Viewable extends IDocument, IViewable { }
 export interface IDocument_Viewable_ByUser extends IDocument_Viewable, IViewable_ByUser { }
@@ -219,7 +221,7 @@ export interface IApprovable<T> {
 }
 export const IApprovable_ByUser_UserIdOfDecider_PropertyName = VC.getTypedPropertyKeyAsText<IApprovable_ByUser<string>>("userIdOfDecider"); // eslint-disable-line camelcase
 export interface IApprovable_ByUser<T> extends IApprovable<T> {
-  readonly userIdOfDecider: string | null;
+  readonly userIdOfDecider: Id | null;
 }
 export interface IDocument_Approvable<T> extends IDocument, IApprovable<T> { }
 export interface IDocument_Approvable_ByUser<T> extends IDocument_Approvable<T>, IApprovable_ByUser<T> { }
@@ -244,8 +246,8 @@ export interface ISuspendable {
 export const ISuspendable_ByUser_UserIdOfSuspensionStarter_PropertyName = VC.getTypedPropertyKeyAsText<ISuspendable_ByUser>("userIdOfSuspensionStarter"); // eslint-disable-line camelcase
 export const ISuspendable_ByUser_UserIdOfSuspensionEnder_PropertyName = VC.getTypedPropertyKeyAsText<ISuspendable_ByUser>("userIdOfSuspensionEnder"); // eslint-disable-line camelcase
 export interface ISuspendable_ByUser extends ISuspendable {
-  readonly userIdOfSuspensionStarter: string | null;
-  readonly userIdOfSuspensionEnder: string | null;
+  readonly userIdOfSuspensionStarter: Id | null;
+  readonly userIdOfSuspensionEnder: Id | null;
 }
 export interface IDocument_Suspendable extends IDocument, ISuspendable { }
 export interface IDocument_Suspendable_ByUser extends IDocument_Suspendable, ISuspendable_ByUser { }
@@ -281,7 +283,7 @@ export const IDeleteable_ByUser_UnDelete_ExactValidSet_PropertyNames: Array<stri
   IDeleteable_ByUser_UserIdOfDeleter_PropertyName,
 ];
 export interface IDeleteable_ByUser extends IDeleteable {
-  readonly userIdOfDeleter: string | null;
+  readonly userIdOfDeleter: Id | null;
 }
 export interface IDocument_Deleteable extends IDocument, IDeleteable { }
 export interface IDocument_Deleteable_ByUser extends IDocument_Deleteable, IDeleteable_ByUser { }
@@ -310,10 +312,10 @@ export function promoteDocumentsToCreatable<IN extends IDocument, OUT extends IN
 
 /**
  * Returns whether the ID is valid.
- * @param {string | null | undefined} id The ID to check.
+ * @param {IdNullable} id The ID to check.
  * @return {boolean}
  */
-export function isIdentifierValid(id: string | null | undefined): boolean {
+export function isIdentifierValid(id: IdNullable): boolean {
   // LATER: Rename "id" to "idSource" and allow caller to pass IDocument (or typed string or IDocument getter) as "idSource"
   const isValid = (!TC.isNullishOrWhitespace(id));
   return isValid;
@@ -321,11 +323,11 @@ export function isIdentifierValid(id: string | null | undefined): boolean {
 
 /**
  * Asserts that the ID is valid.
- * @param {string | null | undefined} id The ID to check.
+ * @param {IdNullable} id The ID to check.
  * @param {string} [message=default] The message to display on failure of assertion.
  * @return {void}
  */
-export function assertIdentifierIsValid(id: string | null | undefined, message = "A valid ID must be provided."): void {
+export function assertIdentifierIsValid(id: IdNullable, message = "A valid ID must be provided."): void {
   // LATER: Rename "id" to "idSource" and allow caller to pass IDocument (or typed string or IDocument getter) as "idSource"
   if (!isIdentifierValid(id)) {
     throw new Error(message);
@@ -336,9 +338,9 @@ export function assertIdentifierIsValid(id: string | null | undefined, message =
  * Gets the valid ID values from the incoming objects using an ID getter function of type T.
  * @param {Array<unknown>} objs The objects to evaluate.
  * @param {IdFunc<T>} idFunc The function that gets the ID value from an object of type T.
- * @return {Array<string>} The resulting list of IDs.
+ * @return {Array<Id>} The resulting list of IDs.
  */
-export function getIdentifiersFromObjects<T>(objs: Array<unknown>, idGetterFunc: IdFunc<T>): Array<string> {
+export function getIdentifiersFromObjects<T>(objs: Array<unknown>, idGetterFunc: IdFunc<T>): Array<Id> {
   TC.assertNonNullish(objs);
   const ids = ([] as Array<string>);
 
