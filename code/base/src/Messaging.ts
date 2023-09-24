6f9f7ae20ed459d9764146783be97e4e31dbf128
rehrenreich <ryanse@gmail.com>
2023-09-24 14:45:31 -0700

    * Refactored getFailureResult(...) function to not assume the object being passed in is actually of type Error, and instead it handles an object of type Error as one of many possible cases. * Added tests for this functionality.

--- code/base/src/Messaging.ts
+++ code/base/src/Messaging.ts
@@ -1,8 +1,17 @@
+import * as TC from "./TypeChecking";
+
 export interface ICallResult<T> {
   readonly success: boolean,
   readonly message: string,
   readonly data: T | undefined
 }
+
+/**
+ * @constant
+ * @type {string}
+ * @default
+ */
+export const UNRECOGNIZED_ERROR_MESSAGE = "An unknown Error occurred"; // eslint-disable-line camelcase
 
 /**
  * @constant
@@ -28,11 +37,45 @@ export function getSuccessResult<T>(message: string, data: T | undefined = undef
 
 /**
  * Creates a result object representing a failed call.
+ * @param {unknown} caught The caught object encountered.
+ * @param {boolean} [includeErrorInResult=Default_IncludeErrorInResult] Whether to return the actual error object as data or not.
+ * @return {ICallResult<unknown>} The result of the call.
+ */
+export function getFailureResult(caught: unknown, includeErrorInResult: boolean = DEFAULT_RESULT_INCLUDES_ERROR): ICallResult<unknown> {
+  if (TC.isNullishOrWhitespace(caught)) {
+    const result: ICallResult<string | null | undefined> = {
+      success: false,
+      message: UNRECOGNIZED_ERROR_MESSAGE,
+      data: (includeErrorInResult) ? (caught as (string | null | undefined)) : undefined,
+    };
+    return result;
+  } else if (TC.isString(caught)) {
+    const result: ICallResult<string> = {
+      success: false,
+      message: (caught as string),
+      data: (includeErrorInResult) ? (caught as string) : undefined,
+    };
+    return result;
+  } else if (TC.isOf<Error>(caught, (value) => (!TC.isNullishOrWhitespace(value.message)))) {
+    const result = getErrorResult(caught as Error, includeErrorInResult);
+    return result;
+  } else {
+    const result: ICallResult<unknown> = {
+      success: false,
+      message: UNRECOGNIZED_ERROR_MESSAGE,
+      data: (includeErrorInResult) ? caught : undefined,
+    };
+    return result;
+  }
+}
+
+/**
+ * Creates a result object representing the error from a failed call.
  * @param {Error} error The error encountered.
  * @param {boolean} [includeErrorInResult=Default_IncludeErrorInResult] Whether to return the actual error object as data or not.
  * @return {ICallResult<Error>} The result of the call.
  */
-export function getFailureResult(error: Error, includeErrorInResult: boolean = DEFAULT_RESULT_INCLUDES_ERROR): ICallResult<Error> {
+export function getErrorResult(error: Error, includeErrorInResult: boolean = DEFAULT_RESULT_INCLUDES_ERROR): ICallResult<Error> {
   const result: ICallResult<Error> = {
     success: false,
     message: error.message,
