7d030a408e574c4738d06bc6cc9305f581f6f499
rehrenreich <ryanse@gmail.com>
2023-09-28 00:58:25 -0700

    * Added test for case where System is not installed.

--- code/domainlogic/src/authorization/Contacts.test.ts
+++ code/domainlogic/src/authorization/Contacts.test.ts
@@ -1,6 +1,6 @@
 import * as admin from "firebase-admin";
-import {afterEach, beforeEach, describe} from "mocha";
-import chai from "chai";
+import {afterEach, beforeEach, describe, test} from "mocha";
+import chai, {expect} from "chai";
 import chaiAsPromised from "chai-as-promised";
 chai.use(chaiAsPromised);
 import * as OPA from "../../../base/src";
@@ -8,8 +8,10 @@ import * as OpaDm from "../../../datamodel/src";
 import {OpaDbDescriptor as OpaDb} from "../../../datamodel/src";
 import * as CSU from "../CallStateUtilities";
 import * as Application from "../system/Application";
+import * as Contacts from "./Contacts";
 import {TestAuthData} from "../TestData.test";
 import * as TestConfig from "../TestConfiguration.test";
+import * as TestUtils from "../TestUtilities.test";
 
 /* eslint-disable brace-style, camelcase */
 
@@ -50,6 +52,24 @@ describe("Contact Tests using Firebase " + config.testEnvironment, function() {
 
     // LATER: Consider terminating DB, deleting App, and re-creating App
   });
+
+  const testFunc1 = () => (async () => {
+    const isSystemInstalled = await Application.isSystemInstalled(config.dataStorageState);
+    expect(isSystemInstalled).equals(false);
+    await TestUtils.assertUserDoesNotExist(config.dataStorageState, config.authenticationState);
+    await TestUtils.assertUserDoesNotExist(config.dataStorageState, TestAuthData.owner);
+
+    // NOTE: Construct the CallState directly because calling the utility function to do so will fail
+    const callState: OpaDm.ICallState = {
+      dataStorageState: config.dataStorageState,
+      authenticationState: config.authenticationState,
+      hasSystemState: false,
+      hasAuthorizationState: false,
+    };
+
+    await expect(Contacts.createContact(callState, null, null, null, null, null, null, null)).to.eventually.be.rejectedWith(Error);
+  });
+  test("checks that createContact(...) fails when System is not installed", testFunc1());
 
   afterEach(async () => {
     await config.dataStorageState.db.terminate();
