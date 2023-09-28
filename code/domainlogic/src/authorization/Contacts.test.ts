995ab3205d6db23fbdfe3b71a39d427ece02f5c9
rehrenreich <ryanse@gmail.com>
2023-09-28 00:54:34 -0700

    * Improved stub for Contacts tests.

--- code/domainlogic/src/authorization/Contacts.test.ts
+++ code/domainlogic/src/authorization/Contacts.test.ts
@@ -1,4 +1,14 @@
+import * as admin from "firebase-admin";
+import {afterEach, beforeEach, describe} from "mocha";
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+chai.use(chaiAsPromised);
 import * as OPA from "../../../base/src";
+import * as OpaDm from "../../../datamodel/src";
+import {OpaDbDescriptor as OpaDb} from "../../../datamodel/src";
+import * as CSU from "../CallStateUtilities";
+import * as Application from "../system/Application";
+import {TestAuthData} from "../TestData.test";
 import * as TestConfig from "../TestConfiguration.test";
 
 /* eslint-disable brace-style, camelcase */
@@ -9,4 +19,40 @@ describe("Contact Tests using Firebase " + config.testEnvironment, function() {
   if (!OPA.isNullish(config.timeout)) {
     this.timeout(OPA.convertNonNullish(config.timeout)); // eslint-disable-line no-invalid-this
   }
+
+  beforeEach(async () => {
+    // NOTE: Make sure that each AuthenticationState object is reset to its original state
+    TestAuthData.resetTestData();
+    // NOTE Set the ambient AuthenticationState to Archive Owner so that beforeEach(...) succeeds
+    config.authenticationState = TestAuthData.owner;
+
+    const doBackup = false && (config.hasRunTests && (config.testEnvironment != "Emulators")); // LATER: Once backup is implemented, delete "false && "
+    config.hasRunTests = false;
+
+    admin.initializeApp(config.appInitializationArgs);
+    config.dataStorageState.db = admin.firestore();
+    config.dataStorageState.currentBulkWriter = null;
+    config.dataStorageState.currentWriteBatch = null;
+
+    const isSystemInstalled = await Application.isSystemInstalled(config.dataStorageState);
+    if (isSystemInstalled) {
+      // NOTE: The following line is necessary bc the System may have been installed in manner other than running tests
+      const ownerForUninstall = await OpaDb.Users.queries.getById(config.dataStorageState, OpaDm.User_OwnerId);
+
+      if (OPA.isNullish(ownerForUninstall)) {
+        // NOTE: Passing a valid value for "authorizationState" only matters if the Archive Owner actually exists
+        await Application.performUninstall(config.dataStorageState, config.authenticationState, null, doBackup);
+      } else {
+        const authorizationState = await CSU.readAuthorizationStateForFirebaseAuthUser(config.dataStorageState, OPA.convertNonNullish(ownerForUninstall).firebaseAuthUserId);
+        await Application.performUninstall(config.dataStorageState, config.authenticationState, authorizationState, doBackup);
+      }
+    }
+
+    // LATER: Consider terminating DB, deleting App, and re-creating App
+  });
+
+  afterEach(async () => {
+    await config.dataStorageState.db.terminate();
+    await admin.app().delete();
+  });
 });
