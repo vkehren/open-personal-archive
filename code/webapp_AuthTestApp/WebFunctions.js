214cabf925bcfc849c3f64f922f1977e0ac954e0
rehrenreich <ryanse@gmail.com>
2023-10-06 15:56:08 -0700

    * Improved WebFunction module by eliminating recursive logging calls and adding a function to submit the User's email address.

--- code/webapp_AuthTestApp/WebFunctions.js
+++ code/webapp_AuthTestApp/WebFunctions.js
@@ -1,11 +1,11 @@
 import {getFunctions, httpsCallable} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-functions.js";
 import {FIREBASE_FUNCTIONS_REGION} from "./WebAppConfig.js";
-import {isNullish} from "./WebBaseLite.js";
+import {isNullish, isBoolean} from "./WebBaseLite.js";
 
 const NAME = "open-personal-archive-web-functions";
-const VERSION = "2.4.0";
+const VERSION = "2.4.1";
 const AUTHOR = "Ryan Stephen Ehrenreich";
-const COPYRIGHT = "Copyright Â© 2021 Open Personal Archiveâ„¢";
+const COPYRIGHT = "Copyright © 2021 Open Personal Archive™";
 
 export const ActivityTypes = {
   _typeName: "ActivityType",
@@ -29,22 +29,60 @@ async function recordPageAction(app, resource = window.location.href, action, da
 
 async function recordPageError(app, error, resource = window.location.href, otherState = null) {
   const data = {error: JSON.stringify(error)};
-  await recordLogItem(app, ActivityTypes.web_page_view, resource, null, data, otherState);
+  await recordLogItem(app, ActivityTypes.web_page_view, resource, null, data, otherState, true);
 }
 
-async function recordLogItem(app, activityType, resource, action, data, otherState = null) {
+async function recordLogItem(app, activityType, resource, action, data, otherState = null, isPageError = false) {
   try {
     const functions = getFunctions(app, FIREBASE_FUNCTIONS_REGION);
     const functionObject = httpsCallable(functions, "recordLogItem");
     const functionArgs = {activityType, resource, action, data, otherState};
     await functionObject(functionArgs);
   } catch (error) {
-    try {
+    console.error((!isNullish(error.message)) ? error.message : error);
+
+    if (!isPageError) {
       await recordPageError();
-    } catch (error) {
-      console.error(error);
     }
   }
 }
 
-export {recordPageLoad, recordPageView, recordPageAction, recordPageError};
+async function submitUserEmail(app, event, contactForm, emailInput, submitButton, waitIndicator, contactResult) {
+  try {
+    event.preventDefault();
+
+    emailInput.disabled = true;
+    submitButton.disabled = true;
+    waitIndicator.style.display = submitButton.style.display;
+    waitIndicator.style.visibility = submitButton.style.visibility;
+    submitButton.style.display = "none";
+    submitButton.style.visibility = "hidden";
+
+    const functionObject = httpsCallable(functions, "createContact");
+    const functionArgs = {email: emailInput.value};
+    const functionResult = await functionObject(functionArgs);
+    const functionSuccess = (isBoolean(functionResult.success) && functionResult.success);
+
+    if (!functionSuccess) {
+      throw new Error("There was an error processing your submission.");
+    }
+
+    contactResult.style.display = contactForm.style.display;
+    contactResult.style.visibility = contactForm.style.visibility;
+    contactForm.style.display = "none";
+    contactForm.style.visibility = "hidden";
+  } catch (error) {
+    emailInput.disabled = false;
+    submitButton.disabled = false;
+    submitButton.style.display = waitIndicator.style.display;
+    submitButton.style.visibility = waitIndicator.style.visibility;
+    waitIndicator.style.display = "none";
+    waitIndicator.style.visibility = "hidden";
+    window.alert("There was an error processing your request... Please try again later.");
+
+    console.error((!isNullish(error.message)) ? error.message : error);
+    await recordPageError();
+  }
+}
+
+export {recordPageLoad, recordPageView, recordPageAction, recordPageError, submitUserEmail};
