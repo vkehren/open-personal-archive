49c22b09fd6af015bfb7307cf1e0ca6a0c50409a
rehrenreich <ryanse@gmail.com>
2023-10-10 03:31:12 -0700

    * Implemented getModuleNameFromSrc(...) and getModuleName(...) functions. * Added tests for these functions.

--- code/base/src/Application.test.ts
+++ code/base/src/Application.test.ts
@@ -2,6 +2,9 @@ import * as AP from "./Application";
 
 /* eslint-disable camelcase */
 export const name = "ApplicationTests";
+const moduleFilename = "/workspace/lib/package_name/src/sub_folder/MyModule.js";
+const moduleNameFromSrc = "/sub_folder/MyModule";
+const moduleName = "MyModule";
 type EnumUnderscore = "a_b" | "b_c" | "c_d";
 type EnumDash = "a-b" | "b-c" | "c-d";
 const undefinedString = ((undefined as unknown) as string);
@@ -10,6 +13,42 @@ const falseString = ((false as unknown) as string);
 const trueString = ((true as unknown) as string);
 const zeroString = ((0 as unknown) as string);
 const oneString = ((1 as unknown) as string);
+
+
+// TESTS for getModuleNameFromSrc(...)
+test("checks that passing 'undefined' throws error", () => {
+  expect(() => AP.getModuleNameFromSrc(undefinedString)).toThrow();
+});
+
+test("checks that passing 'null' throws error", () => {
+  expect(() => AP.getModuleNameFromSrc(nullString)).toThrow();
+});
+
+test("checks that passing '' throws error", () => {
+  expect(() => AP.getModuleNameFromSrc("")).toThrow();
+});
+
+test("checks that passing valid filename returns correct result", () => {
+  expect(AP.getModuleNameFromSrc(moduleFilename)).toEqual(moduleNameFromSrc);
+});
+
+
+// TESTS for getModuleName(...)
+test("checks that passing 'undefined' throws error", () => {
+  expect(() => AP.getModuleName(undefinedString)).toThrow();
+});
+
+test("checks that passing 'null' throws error", () => {
+  expect(() => AP.getModuleName(nullString)).toThrow();
+});
+
+test("checks that passing '' throws error", () => {
+  expect(() => AP.getModuleName("")).toThrow();
+});
+
+test("checks that passing valid filename returns correct result", () => {
+  expect(AP.getModuleName(moduleFilename)).toEqual(moduleName);
+});
 
 
 // TESTS for copyObject(...)
@@ -38,12 +77,12 @@ test("checks that passing 'falseString' returns 'falseString'", () => {
 });
 
 test("checks that passing 'falseString' returns 'falseString'", () => {
-  const input = {a: 1, b: 2, c: true, d: "four"};
+  const input = { a: 1, b: 2, c: true, d: "four" };
   expect(AP.copyObject(input)).toEqual(input);
 });
 
 test("checks that passing 'falseString' returns 'falseString'", () => {
-  const input = {a: 1, b: 2, c: true, d: "four", e: [trueString, falseString], f: {x: "x", y: 2, z: "aaaaaaaa"}};
+  const input = { a: 1, b: 2, c: true, d: "four", e: [trueString, falseString], f: { x: "x", y: 2, z: "aaaaaaaa" } };
   expect(AP.copyObject(input)).toEqual(input);
 });
 
