a9ae35ccc0940b6b9cd2dec2d40290ef68df7455
rehrenreich <ryanse@gmail.com>
2023-10-29 23:04:19 -0700

    * Added function parameters to IQuerySet<T> interface and QuerySet<T> base class to apply date range and paging values from QueryOptions object.

--- code/base/src/Queries.ts
+++ code/base/src/Queries.ts
@@ -7,32 +7,45 @@ import * as ST from "./Storage";
 import * as TC from "./TypeChecking";
 import * as VC from "./ValueChecking";
 
-export interface PagingQueryOptions {
+export type CollectionSource<T> = (firestore.CollectionGroup<T> | firestore.CollectionReference<T>);
+export type QuerySource<T> = (firestore.CollectionGroup<T> | firestore.CollectionReference<T> | firestore.Query<T>);
+
+export interface IPagingQueryOptions {
   limit: number,
   offset?: number,
 }
 
-export interface DateQueryOptions {
+export interface IDateQueryOptions {
   dateFieldName: string | firestore.FieldPath,
   direction?: firestore.OrderByDirection,
   startDate?: BT.DateToUse,
   endDate?: BT.DateToUse,
 }
 
-export interface QueryOptions {
-  pagingOptions?: PagingQueryOptions,
-  dateOptions?: DateQueryOptions,
+export interface IQueryOptions {
+  pagingOptions?: IPagingQueryOptions,
+  dateOptions?: IDateQueryOptions,
 }
 
-export function applyOptionsToQuery<T>(query: firestore.Query<T>, options: QueryOptions = {}): void {
+/**
+ * Applies query options to a Firestore query.
+ * @param {QuerySource<T>} query The query to which to apply the specified options.
+ * @param {IQueryOptions | undefined} queryOptions The query options to apply prior to executing the query.
+ * @return {Promise<Array<T>>} The list of Documents in the Collection.
+ */
+export function applyOptionsToQuery<T>(query: QuerySource<T>, options: IQueryOptions | undefined): QuerySource<T> {
   TC.assertNonNullish(query, "The Query must not be null.");
-  TC.assertNonNullish(options, "The Query Options must not be null.");
 
-  const hasDateOptions = (!TC.isNullish(options.dateOptions));
-  const hasPagingOptions = (!TC.isNullish(options.pagingOptions));
+  if (TC.isNullish(options)) {
+    return query;
+  }
+
+  const optionsNonNull = TC.convertNonNullish(options);
+  const hasDateOptions = (!TC.isNullish(optionsNonNull.dateOptions));
+  const hasPagingOptions = (!TC.isNullish(optionsNonNull.pagingOptions));
 
   if (hasDateOptions) {
-    const dateOptions = TC.convertNonNullish(options.dateOptions);
+    const dateOptions = TC.convertNonNullish(optionsNonNull.dateOptions);
 
     if (!TC.isNullish(dateOptions.direction)) {
       const direction = TC.convertNonNullish(dateOptions.direction);
@@ -48,14 +61,17 @@ export function applyOptionsToQuery<T>(query: firestore.Query<T>, options: Query
       query = query.endAt(dateOptions.endDate);
     }
   }
+
   if (hasPagingOptions) {
-    const pagingOptions = TC.convertNonNullish(options.pagingOptions);
+    const pagingOptions = TC.convertNonNullish(optionsNonNull.pagingOptions);
     query = query.limit(pagingOptions.limit);
     if (!TC.isNullish(pagingOptions.offset)) {
       const offset = TC.convertNonNullish(pagingOptions.offset);
       query = query.offset(offset);
     }
   }
+
+  return query;
 }
 
 export type QuerySetConstructor<Q extends IQuerySet<T>, T extends DT.IDocument> = (collectionDescriptor: ST.ITypedCollectionDescriptor<T>) => Q;
@@ -65,9 +81,9 @@ export interface IQuerySet<T extends DT.IDocument> {
   documentProxyConstructor: BT.ProxyFunc<T>;
   getById(ds: FB.IDataStorageState, id: string): Promise<T | null>;
   getByIdWithAssert(ds: FB.IDataStorageState, id: string, assertionFailureMessage?: string): Promise<T>;
-  getForIds(ds: FB.IDataStorageState, ids: Array<string>, pathFromRoot?: Array<ST.INestedCollectionStep> | undefined): Promise<Array<T>>;
-  getForIdsWithAssert(ds: FB.IDataStorageState, ids: Array<string>, pathFromRoot?: Array<ST.INestedCollectionStep> | undefined, assertionFailureMessage?: string): Promise<Array<T>>;
-  getAll(ds: FB.IDataStorageState, pathFromRoot?: Array<ST.INestedCollectionStep> | undefined): Promise<Array<T>>;
+  getForIds(ds: FB.IDataStorageState, ids: Array<string>, pathFromRoot?: Array<ST.INestedCollectionStep> | undefined, queryOptions?: IQueryOptions | undefined): Promise<Array<T>>;
+  getForIdsWithAssert(ds: FB.IDataStorageState, ids: Array<string>, pathFromRoot?: Array<ST.INestedCollectionStep> | undefined, queryOptions?: IQueryOptions | undefined, assertionFailureMessage?: string): Promise<Array<T>>;
+  getAll(ds: FB.IDataStorageState, pathFromRoot?: Array<ST.INestedCollectionStep> | undefined, queryOptions?: IQueryOptions | undefined): Promise<Array<T>>;
 }
 
 /** Base class for providing queries for a collection. */
@@ -164,9 +180,10 @@ export class QuerySet<T extends DT.IDocument> implements IQuerySet<T> {
    * @param {FB.IDataStorageState} ds The state container for data storage.
    * @param {Array<string>} ids The IDs for the Documents within the OPA system.
    * @param {Array<ST.INestedCollectionStep> | undefined} [pathFromRoot=undefined] The path to the nested Collection from the root of the database.
+   * @param {IQueryOptions | undefined} [queryOptions=undefined] The query options to apply prior to executing the query.
    * @return {Promise<Array<T>>} The Documents corresponding to the IDs.
    */
-  async getForIds(ds: FB.IDataStorageState, ids: Array<string>, pathFromRoot?: Array<ST.INestedCollectionStep> | undefined): Promise<Array<T>> {
+  async getForIds(ds: FB.IDataStorageState, ids: Array<string>, pathFromRoot?: Array<ST.INestedCollectionStep> | undefined, queryOptions?: IQueryOptions | undefined): Promise<Array<T>> {
     FB.assertDataStorageStateIsNotNullish(ds);
     FB.assertFirestoreIsNotNullish(ds.db);
     TC.assertNonNullish(ids);
@@ -176,16 +193,18 @@ export class QuerySet<T extends DT.IDocument> implements IQuerySet<T> {
       return ([] as Array<T>);
     }
 
-    let documentSnaps: Array<firestore.QueryDocumentSnapshot<T>> = [];
+    let collectionSource = ((null as unknown) as CollectionSource<T>);
     if (TC.isNullish(pathFromRoot) && this.collectionDescriptor.isNestedCollection) {
-      const collectionGroup = this.collectionDescriptor.getTypedCollectionGroup(ds);
-      const querySnap = await collectionGroup.where(DT.IDocument_DocumentId_PropertyName, "in", ids).get();
-      documentSnaps = querySnap.docs;
+      collectionSource = this.collectionDescriptor.getTypedCollectionGroup(ds);
     } else {
-      const collectionRef = this.collectionDescriptor.getTypedCollection(ds, pathFromRoot);
-      const querySnap = await collectionRef.where(DT.IDocument_DocumentId_PropertyName, "in", ids).get();
-      documentSnaps = querySnap.docs;
+      collectionSource = this.collectionDescriptor.getTypedCollection(ds, pathFromRoot);;
     }
+
+    let querySource: QuerySource<T>;
+    querySource = collectionSource.where(DT.IDocument_DocumentId_PropertyName, "in", ids);
+    querySource = applyOptionsToQuery(querySource, queryOptions);
+    const querySnap = await querySource.get();
+    const documentSnaps = querySnap.docs;
 
     if (TC.isNullish(documentSnaps)) {
       return ([] as Array<T>);
@@ -204,11 +223,12 @@ export class QuerySet<T extends DT.IDocument> implements IQuerySet<T> {
    * @param {FB.IDataStorageState} ds The state container for data storage.
    * @param {Array<string>} ids The IDs for the Documents within the OPA system.
    * @param {Array<ST.INestedCollectionStep> | undefined} [pathFromRoot=undefined] The path to the nested Collection from the root of the database.
+   * @param {IQueryOptions | undefined} [queryOptions=undefined] The query options to apply prior to executing the query.
    * @param {string} [assertionFailureMessage=default] The message to include in the Error if the assertion fails.
    * @return {Promise<Array<T>>} The Documents corresponding to the IDs.
    */
-  async getForIdsWithAssert(ds: FB.IDataStorageState, ids: Array<string>, pathFromRoot?: Array<ST.INestedCollectionStep> | undefined, assertionFailureMessage = "The resulting list of documents does not match the requested list of IDs."): Promise<Array<T>> {
-    const documents = await this.getForIds(ds, ids, pathFromRoot);
+  async getForIdsWithAssert(ds: FB.IDataStorageState, ids: Array<string>, pathFromRoot?: Array<ST.INestedCollectionStep> | undefined, queryOptions?: IQueryOptions | undefined, assertionFailureMessage = "The resulting list of documents does not match the requested list of IDs."): Promise<Array<T>> {
+    const documents = await this.getForIds(ds, ids, pathFromRoot, queryOptions);
     TC.assertNonNullish(documents, assertionFailureMessage);
     VC.assertIsTrue((documents.length == ids.length), assertionFailureMessage);
     documents.forEach((document) => DT.assertDocumentIsValid(document, assertionFailureMessage, assertionFailureMessage));
@@ -219,23 +239,31 @@ export class QuerySet<T extends DT.IDocument> implements IQuerySet<T> {
    * Gets all Documents in a specific Collection (or Collection Group, if no pathFromRoot is provided for a nested Collection).
    * @param {FB.IDataStorageState} ds The state container for data storage.
    * @param {Array<ST.INestedCollectionStep> | undefined} [pathFromRoot=undefined] The path to the nested Collection from the root of the database.
+   * @param {IQueryOptions | undefined} [queryOptions=undefined] The query options to apply prior to executing the query.
    * @return {Promise<Array<T>>} The list of Documents in the Collection.
    */
-  async getAll(ds: FB.IDataStorageState, pathFromRoot?: Array<ST.INestedCollectionStep> | undefined): Promise<Array<T>> {
+  async getAll(ds: FB.IDataStorageState, pathFromRoot?: Array<ST.INestedCollectionStep> | undefined, queryOptions?: IQueryOptions | undefined): Promise<Array<T>> {
     FB.assertDataStorageStateIsNotNullish(ds);
     FB.assertFirestoreIsNotNullish(ds.db);
 
-    let documents = ([] as Array<T>);
+    let collectionSource = ((null as unknown) as CollectionSource<T>);
     if (TC.isNullish(pathFromRoot) && this.collectionDescriptor.isNestedCollection) {
-      const collectionGroup = this.collectionDescriptor.getTypedCollectionGroup(ds);
-      const querySnap = await collectionGroup.get();
-      documents = querySnap.docs.map((value) => value.data());
+      collectionSource = this.collectionDescriptor.getTypedCollectionGroup(ds);
     } else {
-      const collectionRef = this.collectionDescriptor.getTypedCollection(ds, pathFromRoot);
-      const querySnap = await collectionRef.get();
-      documents = querySnap.docs.map((value) => value.data());
+      collectionSource = this.collectionDescriptor.getTypedCollection(ds, pathFromRoot);;
     }
 
+    let querySource: QuerySource<T>;
+    querySource = collectionSource;
+    querySource = applyOptionsToQuery(querySource, queryOptions);
+    const querySnap = await querySource.get();
+    const documentSnaps = querySnap.docs;
+
+    if (TC.isNullish(documentSnaps)) {
+      return ([] as Array<T>);
+    }
+
+    const documents = documentSnaps.map((value) => value.data());
     const proxiedDocuments = documents.map((document) => this.documentProxyConstructor(document));
     return proxiedDocuments;
   }
